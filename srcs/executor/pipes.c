052a804fbf6c6bc8ecbafcada2851013ad02a417
Oscar GENSER <ogenser@e1r10p16.42.fr>
2021-09-09 18:13:22 +0200

    very basic chained command for pipes

--- srcs/executor/pipes.c
+++ srcs/executor/pipes.c
@@ -6,60 +6,94 @@
 /*   By: ogenser <ogenser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 12:18:17 by ogenser           #+#    #+#             */
-/*   Updated: 2021/09/08 16:37:04 by ogenser          ###   ########.fr       */
+/*   Updated: 2021/09/09 18:11:27 by ogenser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void	ft_pipe(t_mother *s)
+void	ft_pipe(t_mother *s) // note do note foork for certain builtins
 {
-	printf("hello");
-	printf("hello%s\n", s->c->nextpipe->line);
+	// printf("hello");
+	// // printf("hello%s\n", s->c->nextpipe->line);
+	// printf("hello%s, %s\n", s->c->line, s->c->arg[0]);
+	// printf("hello%s, %s\n", s->c->nextpipe->command, s->c->nextpipe->arg[0]);
+
+	int i = 0;
+	while (i < s->pipe)
+	{
+		// printf("hello%s, %s\n", s->c->line, s->c->arg[0]);
+		// s->c = s->c->nextpipe;
+		// int pid = dup2(1, 1);
+		// ft_execfind(s);
+		int status;
+		pid_t pid = fork();
+		if (pid < 0)
+			ft_error(s, "pipe pid is shit", -1);
+		if (pid == 0)
+		{
+			ft_execfind(s);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+		
+		printf("hello %s, %s\n", s->c->line, s->c->arg[0]);
+		s->c = s->c->nextpipe;
+		// printf("hello %s, %s\n", s->c->line, s->c->arg[0]);
+		i++;
+	}
+		// printf("hello%s, %s\n", s->c->line, s->c->arg[0]);
+	// printf("hello%s, %s\n", s->c->nextpipe->command, s->c->nextpipe->arg[0]);
+	
 }
 
 void		multicommands(t_mother *s) 	//sends to different functions if its a pipe redirect etc
 {	
 	//testing:
-	t_command *test = NULL;
+	// char *c = "caca";
 
-printf("hello");
-test = (t_command *)ft_malloc(test, sizeof(test) * 1000);
-test->line = "grep pipe";
-// test->command = ft_strdup("grep");
-test->retvalue = 0;
-test->nbarg = 1;
-test->arg = ft_malloc(test->arg, (sizeof(char *) * 10));
-test->arg[0] = ft_malloc(&test->arg[0], (sizeof(char) * 10));
-test->arg[0] = "pipes.c";
-test->isfollowedbypipe = 0;
-test->nextpipe = NULL;
-test->isprecededbypipe = 1;
-// test->previouspipe = s->c;
+	t_command test;
 
-char *caca = ("cat pipes.c");
-(void)caca;
-s->c->line = caca;//ft_strjoin("", "cat pipes.c");
-s->c->command = "cat";
-s->c->retvalue = 1;
-s->c->nbarg = 1;
-s->c->arg = ft_malloc(&s->c->arg, (sizeof(char *) * 10));
-s->c->arg[0] = ft_malloc(&s->c->arg[0], (sizeof(char) * 10));
-s->c->arg[0] = "pipes.c";
-s->c->isfollowedbypipe = 1;
-s->c->nextpipe = NULL;//&test;
-s->c->isprecededbypipe = 0;
+	test.previouspipe = s->c;
+	test.line = "pwd";
+	test.command = "pwd";
+	test.arg = ft_malloc(test.arg, sizeof(char **) * 10);
+	test.arg[0] = NULL;
+	test.isprecededbypipe = 1;
 
-// s->c->previouspipe = 0;
-// s->c->isoutfile = 0; //boo
-// s->c->outfile = 0;  //pat
-// s->c->isinputfile = 0; //b
-// s->c->inputfile = 0;  //p
-// s->c->iserrofile = 0; //ch
-// s->c->errorfile = 0;
-	// if(s->c->isfollowedbypipe == 1)
-	// 	ft_pipe(s);
-	// if(s->c->isfollowedbypipe == 2)
-	// 	ft_redirect(s);
-	printf("||hello");
+	s->pipe = 2;	
+	s->c->line = "cat pipes.c";
+	s->c->nbarg = 1;
+	s->c->command = "cat";
+	s->c->arg = ft_malloc(s->c->arg, sizeof(char **) * 4);
+	s->c->arg[0] = s->c->command;
+	s->c->arg[1] = "todo.txt";
+	s->c->arg[2] = "README.md";
+	s->c->arg[3] = NULL; 
+	s->c->isfollowedbypipe = 1;
+	s->c->nextpipe = &test;
+
+	// test.previouspipe = s->c;
+	// test.line = "grep pipe";
+	// test.command = "grep";
+	// test.arg = ft_malloc(test.arg, sizeof(char **) * 10);
+	// test.arg[0] = "grep";
+	// test.arg[1] = "pipe";
+	// test.arg[2] = NULL;	
+	// test.isprecededbypipe = 1;
+
+	// char *k[4];
+	// ft_bzero(k[0], sizeof(k[0]));
+	// k[1] = "README.md";
+	// k[2] = "todo.txt";
+	// k[0] = "cat";
+	// k[3] = NULL;
+	// execve("/bin/cat", k, NULL);
+	// ft_execfind(s);
+	
+	if (s->c->isfollowedbypipe == 1)
+		ft_pipe(s);
+	// printf("||hello");
 }
--- srcs/lexer/minilexer.c
+++ srcs/lexer/minilexer.c
@@ -6,7 +6,7 @@
 /*   By: ogenser <ogenser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 12:20:20 by fsacquin          #+#    #+#             */
-/*   Updated: 2021/09/08 11:21:03 by ogenser          ###   ########.fr       */
+/*   Updated: 2021/09/09 13:17:55 by ogenser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,93 +25,101 @@
 // P = pipe
 // S = separator ie : ';'
 
+//ca va sortir comment en fin de parsing? corrige
+
 //moved to ft_lexinit
 ///*
 void    minilexer(t_mother *s)
 {
-    int     i;
-    int     j;
+    (void)s;
+    // int     i;
+    // int     j;
+    // char c; //debug o
+    // void *next;//debug o
+    // void *prev;//debug o
 
-    i = 0;
-    j = 0;
-    next = NULL;
-    prev = NULL;
-    while (s->line[j])
-    {
-        c = '\0';
-        if (s->line[j + 1] == '\0') //if end of input is recognised
-            link_chain_elem(s, &i, &j, 48);
-        else if (s->line[j] == ';') // rule for separator
-        {
-            j--;
-            link_chain_elem(s, &i, &j, 's');
-            link_chain_elem(s, &i, &j, 'S');
-        }
-        else if (ft_strchr("<>+-!=&|^~", s->line[j - 1]) && ft_strchr("<>+-!=&|^~", s->line[j]) && s->lex->quote == 0) //adding operator as part of a bigger operation if not quoted
-            link_chain_elem(s, &i, &j, 'o');
-        else if (ft_strchr(s->line[j - 1], "<>+-!=&|^~") && !(ft_strchr(s->line[j], "<>+-!=&|^~"))) // if prev char is operator but new char is not, then we tokenize previous character
-            link_chain_elem(s, &i, &j, 'o');
-        else if (s->lex->quote == 0 && ft_strchr(s->line[j], "\\\"\'")) //opening a quoting character
-            s->lex->quote = 1;
-        else if (s->lex->quote == 0 && ft_strchr(s->line[j], "$`")) // see parameter expansion and $ behavior
-            unquoted_parameter_expansion(s, i, j);
-        else if (s->lex->quote == 0 && ft_strchr(s->line[j - 1], "<>+-!=&|^~")
-        {
-            j--;
-            link_chain_elem(s, &i, &j, 43);
-        }
-        else if (s->lex->quote == 0 && (s->line[j] == '\n' || s->line[j] == '\r')) // character newline in input forces token creation
-            link_chain_elem(s, &i, &j, 48);
-        // normally, we put here a rule for <blank> characters, but honestly, I really can't be bothered right now.
-        else if (ft_match_word(s->line[j])) // this is for word and characters with no special meaning, we iterate until the word is complete
-            j = j;
-        else if (s->line[j] == '#')
-            s->lex->comment == 1;
-        else
-            i = j;
-        j++;
-    }
+    // i = 0;
+    // j = 0;
+    // next = NULL;
+    // prev = NULL;
+    // while (s->line[j])
+    // {
+    //     c = '\0';
+    //     if (s->line[j + 1] == '\0') //if end of input is recognised
+    //         link_chain_elem(s, &i, &j, 48);
+    //     else if (s->line[j] == ';') // rule for separator
+    //     {
+    //         j--;
+    //         link_chain_elem(s, &i, &j, 's');
+    //         link_chain_elem(s, &i, &j, 'S');
+    //     }
+    //     else if (ft_strchr("<>+-!=&|^~", s->line[j - 1]) && ft_strchr("<>+-!=&|^~", s->line[j]) && s->lex->quote == 0) //adding operator as part of a bigger operation if not quoted
+    //         link_chain_elem(s, &i, &j, 'o');
+    //     else if (ft_strchr(s->line[j - 1], "<>+-!=&|^~") && !(ft_strchr(s->line[j], "<>+-!=&|^~"))) // if prev char is operator but new char is not, then we tokenize previous character
+    //         link_chain_elem(s, &i, &j, 'o');
+    //     else if (s->lex->quote == 0 && ft_strchr(s->line[j], "\\\"\'")) //opening a quoting character
+    //         s->lex->quote = 1;
+    //     else if (s->lex->quote == 0 && ft_strchr(s->line[j], "$`")) // see parameter expansion and $ behavior
+    //         unquoted_parameter_expansion(s, i, j);
+    //     else if (s->lex->quote == 0 && ft_strchr(s->line[j - 1], "<>+-!=&|^~"))
+    //     {
+    //         j--;
+    //         link_chain_elem(s, &i, &j, 43);
+    //     }
+    //     else if (s->lex->quote == 0 && (s->line[j] == '\n' || s->line[j] == '\r')) // character newline in input forces token creation
+    //         link_chain_elem(s, &i, &j, 48);
+    //     // normally, we put here a rule for <blank> characters, but honestly, I really can't be bothered right now.
+    //     else if (ft_match_word(s->line[j])) // this is for word and characters with no special meaning, we iterate until the word is complete
+    //         j = j;
+    //     else if (s->line[j] == '#')
+    //         s->lex->comment == 1;
+    //     else
+    //         i = j;
+    //     j++;
+    // }
 }
 
 void    link_chain_elem(t_mother *s, int *i, int *j, char c)
 {
-    t_token *prev;
-    t_token *next;
+    (void)s; (void)i;(void)j; (void)c;
+    // t_token *prev;
+    // t_token *next;
 
-    prev = ft_last_elem(s->lex->first_token);
-    if (prev->token != NULL)
-    {
-        next = create_token(s, i, j, c);
-        next->prev = prev;
-        prev->next = next;
-        next->next = NULL;
-    }
-    *i = *j;
-    if (c == 43 || c == 's')
-        *j++;
+    // prev = ft_last_elem(s->lex->first_token);
+    // if (prev->token != NULL)
+    // {
+    //     next = create_token(s, i, j, c);
+    //     next->prev = prev;
+    //     prev->next = next;
+    //     next->next = NULL;
+    // }
+    // *i = *j;
+    // if (c == 43 || c == 's')
+    //     *j++;
 }
 
-t_token *create_token(t_mother *s, int i, int j, char c) //creates a new token with content being the characters from s->line going from i to j
-{
-    t_token *new;
+// t_token *create_token(t_mother *s, int i, int j, char c) //creates a new token with content being the characters from s->line going from i to j
+// {
+//     t_token *new;
 
-    if (j <= i)
-    {
-        printf("weird token creation, i = %i and j = %i\n", i, j);
-        new->token = NULL;
-        new->type ='\0';
-    }
-    else
-    {
-        new = ft_malloc(&new, sizeof(t_token));
-        new->token = ft_strdup(s->line, i, j);
-        if (ft_strchr("wbcopPrS", c))
-            new->type = c;
-        else
-            new->type = '\0';
-        }
+//     (void)s; (void)i;(void)j; (void)c;
+//     // if (j <= i)
+//     // {
+//     //     printf("weird token creation, i = %i and j = %i\n", i, j);
+//     //     new->token = NULL;
+//     //     new->type ='\0';
+//     // }
+//     // else
+//     // {
+//     //     new = ft_malloc(&new, sizeof(t_token));
+//     //     // new->token = ft_strdup(s->line, i, j); //debug o
+//     //     if (ft_strchr("wbcopPrS", c))
+//     //         new->type = c;
+//     //     else
+//     //     {    new->type = '\0';
+//     //     }
         
-    }
-    return (new)
-}
+//     // }
+//     return (new);
+// }
 //*/
